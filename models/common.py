2a3c5bfbb464b984d279568ca27ea853c077a941
Salma Zakaria <53764024+SalmaZakaria@users.noreply.github.com>
2023-03-16 04:03:40 +0200

    Update common.py
    Signed-off-by: Salma Zakaria <53764024+SalmaZakaria@users.noreply.github.com>

--- models/common.py
+++ models/common.py
@@ -791,7 +791,7 @@ class Detections:
         self._run(show=True, labels=labels)  # show results
 
     def save(self, labels=True, save_dir='runs/detect/exp', exist_ok=False):
-        save_dir = increment_path(save_dir, exist_ok, mkdir=True)  # increment save_dir
+        save_dir = increment_path(save_dir, exist_ok, mkdir=False)  # increment save_dir
         self._run(save=True, labels=labels, save_dir=save_dir)  # save results
 
     def crop(self, save=True, save_dir='runs/detect/exp', exist_ok=False):
